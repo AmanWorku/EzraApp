fcfc16282dd64579a075b76689b0be960c234d87
AmanWorku <amexworku2014@gmail.com>
2024-01-11 17:01:32 +0300

    Remove unnecessary code segments

--- App.tsx
+++ App.tsx
@@ -23,17 +23,9 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-primary-1">
-      <StatusBar
-        // barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        // backgroundColor={backgroundStyle.backgroundColor}
-        className="bg-accent-6"
-      />
+      <StatusBar className="bg-accent-6" />
       <View>
         <User className="self-center" weight="fill" color="#EA9215" />
         <Text className="text-2xl text-accent-6 font-nokia-light">
@@ -43,14 +35,5 @@ function App(): React.JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  text: {
-    textAlign: 'center',
-    color: 'black',
-    fontSize: 60,
-    fontFamily: 'Nokia Pure Headline Ultra Light',
-  },
-});
 
 export default App;
