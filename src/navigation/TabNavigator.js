c6361abc05985b3d3954ef0a4ee2989cffe18982
AmanWorku <amexworku2014@gmail.com>
2024-01-11 17:32:00 +0300

    Create screens and import them in the tab navigator

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,8 +1,17 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import Home from '../screens/Home';
+import Course from '../screens/Course';
+import Devotion from '../screens/Devotion';
+
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
-  return <div>TabNavigator</div>;
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Course" component={Course} />
+    <Tab.Screen name="Devotion" component={Devotion} />
+  </Tab.Navigator>;
 };
 
 export default TabNavigator;
