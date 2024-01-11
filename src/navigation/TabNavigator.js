5f43dd893ee21e2288364cf46b13455e68ce7506
AmanWorku <amexworku2014@gmail.com>
2024-01-11 17:27:16 +0300

    Create sample files

---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,8 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+
+const TabNavigator = () => {
+  return <div>TabNavigator</div>;
+};
+
+export default TabNavigator;
---
+++ src/screens/Devotion.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Devotion = () => {
+  return <div>Devotion</div>;
+};
+
+export default Devotion;
