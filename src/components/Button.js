6daf9b6694a0cb1399a31f8f61c765f3369d6992
AmanWorku <amexworku2014@gmail.com>
2024-01-14 11:48:48 +0300

    Update button change

--- src/components/Button.js
+++ src/components/Button.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderColor: COLORS.primary,
     borderWidth: 1,
-    borderRadius: 25,
+    borderRadius: 5,
     alignItems: 'center',
     justifyContent: 'center',
   },
