a54cc9b2290d626705146208b5d1cda12131fcc6
AmanWorku <amexworku2014@gmail.com>
2024-01-15 14:00:13 +0300

    Update code

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -7,26 +6,16 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import {styled} from 'nativewind';
-import {Horse, Heart, Cube} from 'phosphor-react-native';
-const StyledView = styled(View);
-const StyledText = styled(Text);
+import {List} from 'phosphor-react-native';
 
 const Home = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  const Box = ({className = '', ...props}) => (
-    <StyledText
-      className={`flex text-center h-14 w-14 justify-center items-center text-white bg-fuchsia-500 rounded ${className}`}
-      {...props}
-    />
-  );
+
   return (
     <SafeAreaView className="flex mx-auto w-[92%]">
-      <StyledView className="flex flex-row justify-between h-screen my-4">
-        <Horse />
-        <Heart color="#AE2983" weight="fill" size={32} />
-        <Cube color="teal" weight="duotone" />
-      </StyledView>
+      <View className="flex flex-row justify-between h-screen my-4">
+        <List size={32} weight="bold" />
+      </View>
     </SafeAreaView>
   );
 };
