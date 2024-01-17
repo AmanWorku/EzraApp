30eab63422675e991ef43e38c442cf7415f8e2b7
AmanWorku <amexworku2014@gmail.com>
2024-01-17 12:04:39 +0300

    Create sample content

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,13 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
+  TouchableOpacity,
   Text,
   TextInput,
   useColorScheme,
   View,
 } from 'react-native';
-import {List, User} from 'phosphor-react-native';
+import {List, User, BookOpenText} from 'phosphor-react-native';
 import tw from './../../tailwind';
 
 const Home = () => {
@@ -15,16 +16,34 @@ const Home = () => {
 
   return (
     <SafeAreaView style={tw`flex mx-auto w-[92%]`}>
-      <View style={tw`flex flex-row justify-between h-screen my-4`}>
+      <View
+        style={tw`flex flex-row justify-between h-screen my-4 text-secondary-6`}>
         <List size={32} weight="bold" style={tw`text-secondary-6`} />
-        <Text style={tw`font-nokia-bold text-xl text-accent-6`}>Home</Text>
+        <Text style={tw`font-nokia-bold text-xl text-secondary-6`}>Home</Text>
         <User size={32} weight="bold" style={tw`text-secondary-6`} />
       </View>
       <View>
         <TextInput
-          placeholder="Search Anything"
+          placeholder="Search anything..."
           style={tw`border border-primary-7 rounded px-4 py-2 font-nokia-bold`}
         />
+      </View>
+      <View
+        style={tw`border-2 border-accent-6 mt-6 rounded-4 bg-primary-6 shadow-md`}>
+        <View
+          style={tw`flex flex-row w-[100%] px-4 py-4 justify-between items-center`}>
+          <BookOpenText size={32} weight="bold" style={tw`text-accent-6`} />
+          <Text style={tw`text-secondary-6 font-nokia-bold text-sm`}>
+            የዕለቱ ጥቅስ
+          </Text>
+          <TouchableOpacity style={tw`bg-accent-6 px-4 py-1 rounded-full`}>
+            <Text style={tw`text-primary-1 font-nokia-bold text-sm`}>Open</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={`border-b-4 border-red-600`}></View>
+        <View>
+          <Text></Text>
+        </View>
       </View>
     </SafeAreaView>
   );
