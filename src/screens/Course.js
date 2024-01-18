2527a4332f6d15d3f819c9705d23857e8b94d1cc
AmanWorku <amexworku2014@gmail.com>
2024-01-18 13:24:51 +0300

    Create sample card and import images

--- src/screens/Course.js
+++ src/screens/Course.js
@@ -17,6 +17,7 @@ import {
   DownloadSimple,
   ShareNetwork,
   CaretCircleDown,
+  Star,
 } from 'phosphor-react-native';
 import tw from './../../tailwind';
 const Course = () => {
@@ -45,6 +46,64 @@ const Course = () => {
               Latest
             </Text>
             <CaretCircleDown size={24} weight="fill" color={'#EA9215'} />
+          </View>
+        </View>
+        <View style={tw`border border-accent-6 mt-4 rounded-4 p-2`}>
+          <View style={tw`h-48`}>
+            <Image
+              source={require('./../assets/bible.png')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
+            የአጠናን ዘዴዎች
+          </Text>
+          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
+            ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
+          </Text>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <TouchableOpacity
+              style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}>
+              <Text
+                style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                ኮርሱን ክፈት
+              </Text>
+            </TouchableOpacity>
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
+                5.0
+              </Text>
+              <Star size={22} weight="fill" color={'#EA9215'} />
+            </View>
+          </View>
+        </View>
+        <View style={tw`border border-accent-6 mt-4 rounded-4 p-2`}>
+          <View style={tw`h-48`}>
+            <Image
+              source={require('./../assets/bible.png')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
+            የአጠናን ዘዴዎች
+          </Text>
+          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
+            ክርስቶስ እና ቤተክርስቲያን በአዲስ ኪዳን
+          </Text>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <TouchableOpacity
+              style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}>
+              <Text
+                style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                ኮርሱን ክፈት
+              </Text>
+            </TouchableOpacity>
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
+                5.0
+              </Text>
+              <Star size={22} weight="fill" color={'#EA9215'} />
+            </View>
           </View>
         </View>
       </ScrollView>
