2040a559a1b12683bc7b5c36646d7fb13ce8ae17
AmanWorku <amexworku2014@gmail.com>
2024-01-18 18:07:42 +0300

    Add Some sample courses

--- src/screens/CoursePages/DisplayCourse.js
+++ src/screens/CoursePages/DisplayCourse.js
@@ -14,6 +14,7 @@ import {
   CaretCircleDown,
   Star,
   ArrowSquareLeft,
+  ArrowSquareRight,
   PencilSimple,
 } from 'phosphor-react-native';
 import tw from './../../../tailwind';
@@ -65,13 +66,118 @@ const DisplayCourse = () => {
         </View>
         <View style={tw`border-b border-accent-6 my-4`}></View>
         <Text
-          style={tw`font-nokia-bold text-secondary-6 text-lg leading-tight text-justify`}>
+          style={tw`font-nokia-bold text-secondary-6 text-lg leading-snug text-justify`}>
           {'   '}
           መጽሃፍ ቅዱስን በተለያየ መንገድ ማጥናት ይቻላል። ነገር ግን ፍሪያማ ከሆኑት መንገዶች መካከል የሚከተሉት ወሳኝ
           ነጥቦችን ይይዛሉ። ከእነዚህም መካከል ሰባቱን አንድ በአንድ … ቪድዮ ጌሞችን ማዘውተር እና የተለያዩ ገጾችን
           መመልከት የታዳጊ ልጆች የለተለት ተግባር እየሆነ መጥⶆል. ሆኖም ግን ይህ ተግባር በጎም መጥፎም ጎኖች አሉት::
           በጎ ተግባር ልንላቸው ከምንችለው ነገሮች መሃከል አንዱ ታዳጊዎችን በእውቀት እንዲዳብሩ ይረዳል::
         </Text>
+        <TouchableOpacity
+          style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}>
+          <Text style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+            ኮርሱን ክፈት
+          </Text>
+        </TouchableOpacity>
+        <View style={tw`border border-accent-6 mt-4 rounded-4 p-2`}>
+          <View style={tw`h-48`}>
+            <Image
+              source={require('./../../assets/church.png')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
+            የአጠናን ዘዴዎች
+          </Text>
+          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
+            ክርስቶስ እና ቤተክርስቲያን በአዲስ ኪዳን
+          </Text>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <TouchableOpacity
+              style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}>
+              <Text
+                style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                ኮርሱን ክፈት
+              </Text>
+            </TouchableOpacity>
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
+                5.0
+              </Text>
+              <Star size={22} weight="fill" color={'#EA9215'} />
+            </View>
+          </View>
+        </View>
+        <View style={tw`border border-accent-6 mt-4 rounded-4 p-2`}>
+          <View style={tw`h-48`}>
+            <Image
+              source={require('./../../assets/worship.jpeg')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
+            የአጠናን ዘዴዎች
+          </Text>
+          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
+            አምልኮ
+          </Text>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <TouchableOpacity
+              style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}>
+              <Text
+                style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                ኮርሱን ክፈት
+              </Text>
+            </TouchableOpacity>
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
+                5.0
+              </Text>
+              <Star size={22} weight="fill" color={'#EA9215'} />
+            </View>
+          </View>
+        </View>
+        <View
+          style={tw`flex flex-row border border-accent-6 mt-4 rounded-4 p-2 gap-2`}>
+          <View style={tw`h-32 w-47%`}>
+            <Image
+              source={require('./../../assets/bible.png')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <View style={tw`w-50%`}>
+            <Text
+              style={tw`font-nokia-bold text-accent-6 text-lg leading-tight`}>
+              Explore more lessons
+            </Text>
+            <View style={tw`mt-2 flex gap-2`}>
+              <TouchableOpacity
+                style={tw`flex flex-row justify-between items-center px-4 py-2 bg-accent-6 rounded-6`}>
+                <Text style={tw`font-nokia-bold text-primary-1 text-lg`}>
+                  Devotionals
+                </Text>
+                <ArrowSquareRight
+                  size={28}
+                  weight="fill"
+                  color={'#FDFDFD'}
+                  style={tw`text-primary-1`}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={tw`flex flex-row items-center justify-between px-4 py-2 bg-accent-6 rounded-6`}>
+                <Text style={tw`font-nokia-bold text-primary-1 text-lg`}>
+                  Quarterly SSls
+                </Text>
+                <ArrowSquareRight
+                  size={28}
+                  weight="fill"
+                  color={'#FDFDFD'}
+                  style={tw`text-primary-1`}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
