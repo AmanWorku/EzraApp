c266fd92c99bb9d788d915dfa9895ed476bb3244
AmanWorku <amexworku2014@gmail.com>
2024-01-18 18:00:05 +0300

    Create sample content on that

--- src/screens/CoursePages/DisplayCourse.js
+++ src/screens/CoursePages/DisplayCourse.js
@@ -14,6 +14,7 @@ import {
   CaretCircleDown,
   Star,
   ArrowSquareLeft,
+  PencilSimple,
 } from 'phosphor-react-native';
 import tw from './../../../tailwind';
 import {useNavigation} from '@react-navigation/native';
@@ -46,9 +47,31 @@ const DisplayCourse = () => {
         </View>
         <Image
           source={require('./../../assets/intro.png')}
-          style={tw`w-98 h-56 rounded-3`}
+          style={tw`w-98 h-56 mt-4`}
           resizeMode="contain"
         />
+        <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-4`}>
+          የአጠናን ዘዴዎች
+        </Text>
+        <Text
+          style={tw`font-nokia-bold text-secondary-6 text-2xl leading-tight`}>
+          ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
+        </Text>
+        <View style={tw`flex flex-row items-center gap-2 mt-2`}>
+          <PencilSimple size={24} weight="fill" color={'#EA9215'} />
+          <Text style={tw`font-nokia-bold text-accent-6 text-lg`}>
+            ፓ/ር መልዓክ አለማየሁ
+          </Text>
+        </View>
+        <View style={tw`border-b border-accent-6 my-4`}></View>
+        <Text
+          style={tw`font-nokia-bold text-secondary-6 text-lg leading-tight text-justify`}>
+          {'   '}
+          መጽሃፍ ቅዱስን በተለያየ መንገድ ማጥናት ይቻላል። ነገር ግን ፍሪያማ ከሆኑት መንገዶች መካከል የሚከተሉት ወሳኝ
+          ነጥቦችን ይይዛሉ። ከእነዚህም መካከል ሰባቱን አንድ በአንድ … ቪድዮ ጌሞችን ማዘውተር እና የተለያዩ ገጾችን
+          መመልከት የታዳጊ ልጆች የለተለት ተግባር እየሆነ መጥⶆል. ሆኖም ግን ይህ ተግባር በጎም መጥፎም ጎኖች አሉት::
+          በጎ ተግባር ልንላቸው ከምንችለው ነገሮች መሃከል አንዱ ታዳጊዎችን በእውቀት እንዲዳብሩ ይረዳል::
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
