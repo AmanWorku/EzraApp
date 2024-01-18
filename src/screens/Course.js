e8fb0b4b339f87c4c24d8c47a39238399386628b
AmanWorku <amexworku2014@gmail.com>
2024-01-18 13:47:27 +0300

    Finish adding cards to the page

--- src/screens/Course.js
+++ src/screens/Course.js
@@ -18,6 +18,7 @@ import {
   ShareNetwork,
   CaretCircleDown,
   Star,
+  ArrowSquareRight,
 } from 'phosphor-react-native';
 import tw from './../../tailwind';
 const Course = () => {
@@ -118,6 +119,105 @@ const Course = () => {
           </Text>
           <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
             አምልኮ
+          </Text>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <TouchableOpacity
+              style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}>
+              <Text
+                style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                ኮርሱን ክፈት
+              </Text>
+            </TouchableOpacity>
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
+                5.0
+              </Text>
+              <Star size={22} weight="fill" color={'#EA9215'} />
+            </View>
+          </View>
+        </View>
+        <View
+          style={tw`flex flex-row border border-accent-6 mt-4 rounded-4 p-2 gap-2`}>
+          <View style={tw`h-32 w-47%`}>
+            <Image
+              source={require('./../assets/bible.png')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <View style={tw`w-50%`}>
+            <Text
+              style={tw`font-nokia-bold text-accent-6 text-lg leading-tight`}>
+              Explore more lessons
+            </Text>
+            <View style={tw`mt-2 flex gap-2`}>
+              <TouchableOpacity
+                style={tw`flex flex-row justify-between items-center px-4 py-2 bg-accent-6 rounded-6`}>
+                <Text style={tw`font-nokia-bold text-primary-1 text-lg`}>
+                  Devotionals
+                </Text>
+                <ArrowSquareRight
+                  size={28}
+                  weight="fill"
+                  color={'#FDFDFD'}
+                  style={tw`text-primary-1`}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={tw`flex flex-row items-center justify-between px-4 py-2 bg-accent-6 rounded-6`}>
+                <Text style={tw`font-nokia-bold text-primary-1 text-lg`}>
+                  Quarterly SSls
+                </Text>
+                <ArrowSquareRight
+                  size={28}
+                  weight="fill"
+                  color={'#FDFDFD'}
+                  style={tw`text-primary-1`}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={tw`border border-accent-6 mt-4 rounded-4 p-2`}>
+          <View style={tw`h-48`}>
+            <Image
+              source={require('./../assets/wedding.jpeg')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
+            የአጠናን ዘዴዎች
+          </Text>
+          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
+            ስነ-ትዳር
+          </Text>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <TouchableOpacity
+              style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}>
+              <Text
+                style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                ኮርሱን ክፈት
+              </Text>
+            </TouchableOpacity>
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
+                5.0
+              </Text>
+              <Star size={22} weight="fill" color={'#EA9215'} />
+            </View>
+          </View>
+        </View>
+        <View style={tw`border border-accent-6 my-4 rounded-4 p-2`}>
+          <View style={tw`h-48`}>
+            <Image
+              source={require('./../assets/baptism.jpeg')}
+              style={tw`w-full h-full rounded-3`}
+            />
+          </View>
+          <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
+            የአጠናን ዘዴዎች
+          </Text>
+          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
+            ጥምቀት፣ ለምን…? በማን...? መቼ...?
           </Text>
           <View style={tw`flex flex-row items-center justify-between`}>
             <TouchableOpacity
