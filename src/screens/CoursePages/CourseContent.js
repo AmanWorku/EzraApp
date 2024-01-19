f1a68cb8ba89d2e2e531796d2b5ad5ab247c9e94
AmanWorku <amexworku2014@gmail.com>
2024-01-19 12:14:15 +0300

    Add simple styling and image at top

--- src/screens/CoursePages/CourseContent.js
+++ src/screens/CoursePages/CourseContent.js
@@ -8,17 +8,37 @@ import {
 import React from 'react';
 import tw from './../../../tailwind';
 import {useNavigation} from '@react-navigation/native';
+import {ArrowSquareLeft} from 'phosphor-react-native';
 
 const CourseContent = () => {
   return (
     <SafeAreaView>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={tw`h-48`}>
+        <View style={tw`flex-1 h-70`}>
           <ImageBackground
             source={require('./../../assets/bible2.jpeg')}
-            style={tw`h-48 w-100%`}>
-            <Text>Check</Text>
+            style={tw`flex-5`}>
+            <ArrowSquareLeft
+              size={36}
+              weight="fill"
+              color={'#EA9215'}
+              style={tw`px-8 mt-4`}
+            />
           </ImageBackground>
+          <View style={tw`flex-1 bg-primary-7 flex-row gap-2 items-center`}>
+            <View style={tw`ml-6 p-1 bg-accent-6 rounded-1`}>
+              <Text style={tw`font-nokia-bold text-primary-1`}>10%</Text>
+            </View>
+            <Text>Pass 100% of your lessons to complete this course</Text>
+          </View>
+        </View>
+        <View style={tw`flex mx-auto w-[92%]`}>
+          <Text>ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች</Text>
+          <Text>
+            {' '}
+            መጽሃፍ ቅዱስን በተለያየ መንገድ ማጥናት ይቻላል። ነገር ግን ፍሪያማ ከሆኑት መንገዶች መካከል የሚከተሉት
+            ወሳኝ ነጥቦችን ይይዛሉ። ከእነዚህም... More{' '}
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
