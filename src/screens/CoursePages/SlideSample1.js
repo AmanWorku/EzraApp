a85e5aa1665834d85bf78c637708abb196cf9506
AmanWorku <amexworku2014@gmail.com>
2024-01-22 16:50:43 +0300

    Create background for course displaying

--- src/screens/CoursePages/SlideSample1.js
+++ src/screens/CoursePages/SlideSample1.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import {Text, View, StyleSheet, Dimensions} from 'react-native';
-
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ImageBackground,
+  ScrollView,
+} from 'react-native';
+import tw from './../../../tailwind';
 const SlideSample1 = () => {
   return (
-    <View style={styles.fullScreen}>
-      <Text>Sample text</Text>
-    </View>
+    <SafeAreaView style={tw`flex-1`}>
+      <ImageBackground
+        source={require('./../../assets/day4.jpeg')}
+        style={tw`flex-1 p-2`}>
+        <View style={tw`absolute inset-0 bg-accent-9 bg-opacity-70`} />
+
+        <ScrollView contentContainerStyle={tw`flex-grow`}>
+          <Text style={tw`font-nokia-bold mt-22 text-primary-1 text-lg`}>
+            ታህሳስ 16
+          </Text>
+        </ScrollView>
+      </ImageBackground>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  fullScreen: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
 
 export default SlideSample1;
