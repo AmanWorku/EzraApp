e321c3be6bdf8823d72eb707e5b8fea092ca0d34
AmanWorku <amexworku2014@gmail.com>
2024-01-22 17:53:21 +0300

    Style the header section

--- src/screens/CoursePages/SlideSample1.js
+++ src/screens/CoursePages/SlideSample1.js
@@ -8,6 +8,7 @@ import {
   StatusBar,
 } from 'react-native';
 import tw from './../../../tailwind';
+import {DotsThreeVertical} from 'phosphor-react-native';
 const SlideSample1 = () => {
   StatusBar.setHidden(true);
   return (
@@ -16,13 +17,25 @@ const SlideSample1 = () => {
         source={require('./../../assets/day4.jpeg')}
         style={tw`flex-1 p-2`}>
         <View style={tw`absolute inset-0 bg-accent-9 bg-opacity-70`} />
-        <ScrollView contentContainerStyle={tw`flex-grow pt-3 px-2`}>
-          <View style={tw`flex flex-row`}>
-            <Image
-              source={require('./../../assets/LogoSmall.png')}
-              style={tw`w-24`}
-              resizeMode="contain"
-            />
+        <ScrollView contentContainerStyle={tw`flex-grow pt-8 px-2`}>
+          <View style={tw`flex flex-row items-center justify-between`}>
+            <View style={tw`flex flex-row items-center gap-3`}>
+              <Image
+                source={require('./../../assets/LogoSmall.png')}
+                style={tw`w-22 h-11 border-r border-primary-1`}
+                resizeMode="contain"
+              />
+              <View style={tw`border-r border-primary-1 h-[90%]`} />
+              <Text style={tw`font-nokia-bold text-primary-1 text-sm`}>
+                ክፍል ሦስት - የጥሞና ጥናት
+              </Text>
+            </View>
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Text style={tw`font-nokia-bold text-primary-1 text-lg`}>
+                1/15
+              </Text>
+              <DotsThreeVertical weight="bold" style={tw`text-accent-6`} />
+            </View>
           </View>
         </ScrollView>
       </ImageBackground>
