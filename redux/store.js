09b20657081791f74b7a48d7624bffd7c7626792
AmanWorku <amexworku2014@gmail.com>
2024-01-24 11:06:03 +0300

    Import Slice from web

---
+++ redux/store.js
@@ -0,0 +1,16 @@
+import {configureStore} from '@reduxjs/toolkit';
+import courseReducer from './courseSlice';
+import {api} from '../services/api';
+
+const store = configureStore({
+  reducer: {
+    course: courseReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+console.log(store.getState());
+
+export default store;
