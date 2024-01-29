536c2374407768f52a103bdb35e102898380d71a
AmanWorku <amexworku2014@gmail.com>
2024-01-29 21:56:34 +0300

    Create coming soon screen for ssl

--- src/screens/SSL.js
+++ src/screens/SSL.js
@@ -1,10 +1,17 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import tw from './../../tailwind';
 
 const SSL = () => {
   return (
-    <View>
-      <Text>SSL</Text>
+    <View style={tw`flex-1 items-center justify-center gap-2 bg-primary-1`}>
+      <Text style={tw`font-nokia-bold text-secondary-6 text-xl`}>
+        Sabbath School Lesson
+      </Text>
+      <View style={tw`h-0.2 bg-accent-6 w-60%`} />
+      <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
+        Page Under Construction
+      </Text>
     </View>
   );
 };
