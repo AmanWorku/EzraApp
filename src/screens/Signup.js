07ed8310418298c86f5d40c5c80b602bef4342df
AmanWorku <amexworku2014@gmail.com>
2024-01-29 15:58:19 +0300

    Update view and styling

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -13,63 +13,55 @@ import COLORS from '../constants/colors';
 import Button from '../components/Button';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 import {Eye} from 'phosphor-react-native';
+import tw from './../../tailwind';
 
 const icon = <FontAwesome6 name={'comments'} />;
 
 const Signup = ({navigation}) => {
   const [isPasswordShown, setIsPasswordShown] = useState(false);
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
-      <ScrollView style={{flex: 1, marginHorizontal: 22}}>
-        <View style={{marginVertical: 22}}>
-          <Text
-            style={{
-              fontSize: 42,
-              fontWeight: 'bold',
-              marginVertical: 12,
-              color: COLORS.black,
-            }}>
-            Create an
-          </Text>
-          <Text
-            style={{
-              fontSize: 42,
-              fontWeight: 'bold',
-              color: COLORS.black,
-            }}>
-            account
+    <SafeAreaView style={tw`flex-1 bg-primary-1`}>
+      <ScrollView
+        style={tw`flex mx-auto w-[92%]`}
+        showsVerticalScrollIndicator={false}>
+        <View style={tw`my-8`}>
+          <Text style={tw`font-nokia-bold text-6xl text-secondary-6 w-80%`}>
+            Create an account
           </Text>
         </View>
-
-        <View style={{marginBottom: 12}}>
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: 400,
-              marginVertical: 8,
-            }}>
-            Email address
-          </Text>
-
-          <View
-            style={{
-              width: '100%',
-              height: 48,
-              borderColor: COLORS.black,
-              borderWidth: 1,
-              borderRadius: 8,
-              alignItems: 'center',
-              justifyContent: 'center',
-              paddingLeft: 22,
-            }}>
-            <TextInput
-              placeholder="Enter your email address"
-              placeholderTextColor={COLORS.black}
-              keyboardType="email-address"
-              style={{
-                width: '100%',
-              }}
-            />
+        <View style={tw`flex flex-col gap-4`}>
+          <View style={tw`mb-2`}>
+            <View
+              style={tw`w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              <TextInput
+                placeholder="Email address"
+                placeholderTextColor={COLORS.black}
+                keyboardType="email-address"
+                style={tw`placeholder:text-secondary-3 font-nokia-bold text-sm`}
+              />
+            </View>
+          </View>
+          <View style={tw`mb-2`}>
+            <View
+              style={tw`w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              <TextInput
+                placeholder="Email address"
+                placeholderTextColor={COLORS.black}
+                keyboardType="email-address"
+                style={tw`placeholder:text-secondary-3 font-nokia-bold text-sm`}
+              />
+            </View>
+          </View>
+          <View style={tw`mb-2`}>
+            <View
+              style={tw`w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              <TextInput
+                placeholder="Email address"
+                placeholderTextColor={COLORS.black}
+                keyboardType="email-address"
+                style={tw`placeholder:text-secondary-3 font-nokia-bold text-sm`}
+              />
+            </View>
           </View>
         </View>
 
