7907135c7da66387c727c30afd27877aaaca2175
AmanWorku <amexworku2014@gmail.com>
2024-01-29 16:45:47 +0300

    Update sign up form

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -12,7 +12,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import COLORS from '../constants/colors';
 import Button from '../components/Button';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
-import {Eye} from 'phosphor-react-native';
+import {Eye, Lock, UserCircle} from 'phosphor-react-native';
 import tw from './../../tailwind';
 
 const icon = <FontAwesome6 name={'comments'} />;
@@ -32,161 +32,55 @@ const Signup = ({navigation}) => {
         <View style={tw`flex flex-col gap-4`}>
           <View style={tw`mb-2`}>
             <View
-              style={tw`w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              style={tw`flex flex-row items-center gap-2 w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              <UserCircle size={20} style={tw`text-secondary-5`} />
               <TextInput
                 placeholder="Email address"
-                placeholderTextColor={COLORS.black}
                 keyboardType="email-address"
-                style={tw`placeholder:text-secondary-3 font-nokia-bold text-sm`}
+                style={tw`placeholder:text-secondary-5 font-nokia-bold text-sm`}
               />
             </View>
           </View>
           <View style={tw`mb-2`}>
             <View
-              style={tw`w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              style={tw`flex flex-row items-center gap-2 w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              <Lock size={20} style={tw`text-secondary-5`} />
               <TextInput
-                placeholder="Email address"
-                placeholderTextColor={COLORS.black}
+                placeholder="Password"
                 keyboardType="email-address"
-                style={tw`placeholder:text-secondary-3 font-nokia-bold text-sm`}
+                style={tw`placeholder:text-secondary-5 font-nokia-bold text-sm`}
               />
             </View>
           </View>
           <View style={tw`mb-2`}>
             <View
-              style={tw`w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              style={tw`flex flex-row items-center gap-2 w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`}>
+              <Lock size={20} style={tw`text-secondary-5`} />
               <TextInput
-                placeholder="Email address"
-                placeholderTextColor={COLORS.black}
+                placeholder="Confirm Password"
                 keyboardType="email-address"
                 style={tw`placeholder:text-secondary-3 font-nokia-bold text-sm`}
               />
             </View>
           </View>
         </View>
-
-        <View style={{marginBottom: 12}}>
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: 400,
-              marginVertical: 8,
-            }}>
-            Password
+        <Text style={tw`py-2 font-Lato-Bold text-secondary-6`}>
+          By clicking the Sign Up button, you agree to the public offer.
+        </Text>
+        <TouchableOpacity
+          style={tw`w-100% py-4 items-center bg-accent-6 rounded-2 my-2`}
+          onPress={() => navigation.navigate('MainTab')}>
+          <Text style={tw`font-Lato-Black text-primary-1 `}>
+            Create Account
           </Text>
+        </TouchableOpacity>
 
-          <View
-            style={{
-              width: '100%',
-              height: 48,
-              borderColor: COLORS.black,
-              borderWidth: 1,
-              borderRadius: 8,
-              alignItems: 'center',
-              justifyContent: 'center',
-              paddingLeft: 22,
-            }}>
-            <TextInput
-              placeholder="Enter your password"
-              placeholderTextColor={COLORS.black}
-              secureTextEntry={isPasswordShown}
-              style={{
-                width: '100%',
-              }}
-            />
-
-            <TouchableOpacity
-              onPress={() => setIsPasswordShown(!isPasswordShown)}
-              style={{
-                position: 'absolute',
-                right: 12,
-              }}>
-              <Eye size={20} weight="fill" />
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={{marginBottom: 12}}>
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: 400,
-              marginVertical: 8,
-            }}>
-            Confirm Password
+        <View style={tw`flex-row my-4 items-center mx-8`}>
+          <View style={tw`flex-1 h-0.5 bg-secondary-3 mx-2`} />
+          <Text style={tw`font-Lato-Regular text-secondary-6`}>
+            OR Sign up with
           </Text>
-
-          <View
-            style={{
-              width: '100%',
-              height: 48,
-              borderColor: COLORS.black,
-              borderWidth: 1,
-              borderRadius: 8,
-              alignItems: 'center',
-              justifyContent: 'center',
-              paddingLeft: 22,
-            }}>
-            <TextInput
-              placeholder="Confirm your password"
-              placeholderTextColor={COLORS.black}
-              secureTextEntry={isPasswordShown}
-              style={{
-                width: '100%',
-              }}
-            />
-
-            <TouchableOpacity
-              onPress={() => setIsPasswordShown(!isPasswordShown)}
-              style={{
-                position: 'absolute',
-                right: 12,
-              }}>
-              <Eye size={20} weight="fill" />
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        <View
-          style={{
-            flexDirection: 'row',
-            marginVertical: 6,
-          }}>
-          <Text>I agree to the terms and conditions</Text>
-        </View>
-
-        <Button
-          title="Sign Up"
-          onPress={() => navigation.navigate('MainTab')}
-          filled
-          style={{
-            marginTop: 18,
-            marginBottom: 4,
-          }}
-        />
-
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginVertical: 20,
-          }}>
-          <View
-            style={{
-              flex: 1,
-              height: 1,
-              backgroundColor: COLORS.grey,
-              marginHorizontal: 10,
-            }}
-          />
-          <Text style={{fontSize: 14}}>Or Sign up with</Text>
-          <View
-            style={{
-              flex: 1,
-              height: 1,
-              backgroundColor: COLORS.grey,
-              marginHorizontal: 10,
-            }}
-          />
+          <View style={tw`flex-1 h-0.5 bg-secondary-3 mx-2`} />
         </View>
 
         <View
