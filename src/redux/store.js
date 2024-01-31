af80c0ca3edbcc0e5c9b3a20573d9aad5fa69bec
AmanWorku <amexworku2014@gmail.com>
2024-01-31 21:43:50 +0300

    Add dark mode settings

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import courseReducer from './courseSlice';
 import authReducer from './authSlice';
+import uiReducer from './uiSlice';
 import {apiSlice} from './api-slices/apiSlice';
 
 const store = configureStore({
   reducer: {
+    ui: uiReducer,
     course: courseReducer,
     auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -1,17 +1,29 @@
-import {View, Text} from 'react-native';
+import {View, Text, Switch} from 'react-native';
 import React from 'react';
 import tw from './../../tailwind';
 import {useSelector, useDispatch} from 'react-redux';
 import {toggleDarkMode} from '../redux/uiSlice';
 
 const Setting = () => {
+  const dispatch = useDispatch();
+  const darkModeEnabled = useSelector(state => state.ui.darkMode);
+
+  const handleToggle = () => {
+    dispatch(toggleDarkMode());
+  };
+
   return (
-    <View style={tw`flex-1 items-center justify-center gap-2 bg-primary-1`}>
+    <View
+      style={[
+        tw`flex-1 items-center justify-center gap-2 bg-primary-1`,
+        darkModeEnabled ? tw`bg-secondary-9` : tw``,
+      ]}>
       <Text style={tw`font-nokia-bold text-secondary-6 text-xl`}>Settings</Text>
       <View style={tw`h-0.2 bg-accent-6 w-60%`} />
       <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
         Page Under Construction
       </Text>
+      <Switch onValueChange={handleToggle} value={darkModeEnabled} />
     </View>
   );
 };
