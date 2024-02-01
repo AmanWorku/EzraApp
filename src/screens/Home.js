d29cde17801c99831ac03454a5fa4583c85541f2
AmanWorku <amexworku2014@gmail.com>
2024-02-01 03:04:58 +0300

    Finish adding darkmode adjustments to home page.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -17,71 +17,120 @@ import {
   BookOpenText,
   ArrowSquareUpRight,
 } from 'phosphor-react-native';
+import {useSelector} from 'react-redux';
 import tw from './../../tailwind';
 import {useNavigation} from '@react-navigation/native';
 
 const Home = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const navigation = useNavigation();
+  const darkMode = useSelector(state => state.ui.darkMode);
 
   const handleButtonPress = () => {
     navigation.navigate('CourseContent');
   };
   return (
-    <SafeAreaView style={tw`flex mx-auto w-[92%]`}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={tw`flex flex-row justify-between my-4 text-secondary-6`}>
-          <List size={32} weight="bold" style={tw`text-secondary-6`} />
-          <Text style={tw`font-nokia-bold text-xl text-secondary-6`}>Home</Text>
-          <User size={32} weight="bold" style={tw`text-secondary-6`} />
-        </View>
-        <View>
-          <TextInput
-            placeholder="Search anything..."
-            style={tw`border border-primary-7 rounded px-4 py-2 font-nokia-bold`}
-          />
-        </View>
-        <View
-          style={tw`border-2 border-accent-6 mt-6 rounded-4 bg-primary-6 shadow-lg px-4 py-4`}>
-          <View style={tw`flex flex-row w-[100%] justify-between items-center`}>
-            <View style={tw`flex flex-row items-center gap-2`}>
-              <BookOpenText size={32} weight="bold" style={tw`text-accent-6`} />
-              <Text style={tw`text-secondary-6 font-nokia-bold text-lg`}>
-                የዕለቱ ጥቅስ
+    <View style={darkMode ? tw`bg-secondary-9` : null}>
+      <SafeAreaView style={tw`flex mx-auto w-[92%]`}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={tw`flex flex-row justify-between my-4`}>
+            <List
+              size={32}
+              weight="bold"
+              style={[
+                tw`text-secondary-6`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}
+            />
+            <Text
+              style={[
+                tw`font-nokia-bold text-xl text-secondary-6`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}>
+              Home
+            </Text>
+            <User
+              size={32}
+              weight="bold"
+              style={[
+                tw`text-secondary-6`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}
+            />
+          </View>
+          <View>
+            <TextInput
+              placeholder="Search anything..."
+              style={[
+                tw`border border-primary-7 rounded px-4 py-2 font-nokia-bold`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}
+              placeholderTextColor={darkMode ? '#898989' : '#AAB0B4'}
+            />
+          </View>
+          <View
+            style={[
+              tw`border-2 border-accent-6 mt-6 rounded-4 bg-primary-6 shadow-lg px-4 py-4`,
+              darkMode ? tw`bg-secondary-8` : null,
+            ]}>
+            <View
+              style={tw`flex flex-row w-[100%] justify-between items-center`}>
+              <View style={tw`flex flex-row items-center gap-2`}>
+                <BookOpenText
+                  size={32}
+                  weight="bold"
+                  style={tw`text-accent-6`}
+                />
+                <Text
+                  style={[
+                    tw`text-secondary-6 font-nokia-bold text-lg`,
+                    darkMode ? tw`text-primary-2` : null,
+                  ]}>
+                  የዕለቱ ጥቅስ
+                </Text>
+              </View>
+              <TouchableOpacity style={tw`bg-accent-6 px-4 py-1 rounded-full`}>
+                <Text style={tw`text-primary-1 font-nokia-bold text-sm`}>
+                  Open
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={tw`border-b border-accent-6 mt-2 mb-1`} />
+            <View>
+              <Text
+                style={[
+                  tw`font-nokia-bold text-lg text-secondary-6`,
+                  darkMode ? tw`text-primary-2` : null,
+                ]}>
+                “የሌላውን ሕይወት መቤዠት የሚችል ሰው፣ ወይም ለእግዚአብሔር ወጆ የሚከፍልለት ማንም የለም። የነፍስ
+                ቤዛ ውድ ነውና፤”
+              </Text>
+              <Text
+                style={[
+                  tw`font-nokia-light text-lg text-secondary-6`,
+                  darkMode ? tw`text-primary-2` : null,
+                ]}>
+                መዝሙር 49:7፣8
               </Text>
             </View>
-            <TouchableOpacity style={tw`bg-accent-6 px-4 py-1 rounded-full`}>
-              <Text style={tw`text-primary-1 font-nokia-bold text-sm`}>
-                Open
+          </View>
+          <View style={tw`border-b border-primary-7 mt-4 mb-2`} />
+          <View style={tw`flex flex-row justify-between items-center`}>
+            <Text
+              style={[
+                tw`font-nokia-bold text-secondary-5 text-lg`,
+                darkMode ? tw`text-primary-7` : null,
+              ]}>
+              Continue Studying
+            </Text>
+            <TouchableOpacity
+              style={tw`border border-accent-6 px-4 py-1 rounded-4`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
+                All Courses
               </Text>
             </TouchableOpacity>
           </View>
-          <View style={tw`border-b border-accent-6 mt-2 mb-1`} />
-          <View>
-            <Text style={tw`font-nokia-bold text-lg text-secondary-6`}>
-              “የሌላውን ሕይወት መቤዠት የሚችል ሰው፣ ወይም ለእግዚአብሔር ወጆ የሚከፍልለት ማንም የለም። የነፍስ ቤዛ
-              ውድ ነውና፤”
-            </Text>
-            <Text style={tw`font-nokia-light text-lg text-secondary-6`}>
-              መዝሙር 49:7፣8
-            </Text>
-          </View>
-        </View>
-        <View style={tw`border-b border-primary-7 mt-4 mb-2`} />
-        <View style={tw`flex flex-row justify-between items-center`}>
-          <Text style={tw`font-nokia-bold text-secondary-4 text-lg`}>
-            Continue Studying
-          </Text>
-          <TouchableOpacity
-            style={tw`border border-accent-6 px-4 py-1 rounded-4`}>
-            <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
-              All Courses
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={tw`border border-accent-6 mt-4 rounded-4 p-2`}>
-          {/* <ScrollView
+          <View style={tw`border border-accent-6 mt-4 rounded-4 p-2`}>
+            {/* <ScrollView
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           style={tw`mt-4`}>
@@ -104,120 +153,106 @@ const Home = () => {
             </View>
           </View>
         </ScrollView> */}
-          <View style={tw`h-48`}>
-            <Image
-              source={require('./../assets/bible.png')}
-              style={tw`w-full h-full rounded-3`}
-            />
-          </View>
-          <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
-            የአጠናን ዘዴዎች
-          </Text>
-          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
-            ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
-          </Text>
-          <TouchableOpacity
-            style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}
-            onPress={handleButtonPress}>
-            <Text
-              style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
-              ኮርሱን ክፈት
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={tw`border-b border-primary-7 mt-4 mb-2`} />
-        <View style={tw`flex flex-row justify-between items-center`}>
-          <Text style={tw`font-nokia-bold text-secondary-4 text-lg`}>
-            Study this week's SSL
-          </Text>
-          <TouchableOpacity
-            style={tw`border border-accent-6 px-4 py-1 rounded-4`}>
-            <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
-              All SSLs
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={tw`flex flex-row border border-accent-6 mt-4 rounded-4 p-2 gap-2`}>
-          <View style={tw`h-32 w-32`}>
-            <Image
-              source={require('./../assets/bible.png')}
-              style={tw`w-full h-full rounded-3`}
-            />
-          </View>
-          <View style={tw`w-65%`}>
-            <Text
-              style={tw`font-nokia-bold text-accent-6 text-sm leading-tight`}>
+            <View style={tw`h-48`}>
+              <Image
+                source={require('./../assets/bible.png')}
+                style={tw`w-full h-full rounded-3`}
+              />
+            </View>
+            <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
               የአጠናን ዘዴዎች
             </Text>
             <Text
-              style={tw`font-nokia-bold text-secondary-6 text-lg leading-tight`}>
+              style={[
+                tw`font-nokia-bold text-secondary-6 text-2xl`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}>
               ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
             </Text>
-            <View style={tw`border-b border-accent-6 mt-1 w-[63%]`} />
-            <Text style={tw`font-nokia-bold text-secondary-5 text-xs`}>
-              ጥቅምት 01 - ጥቅምት 07
-            </Text>
             <TouchableOpacity
-              style={tw`bg-accent-6 px-4 py-1 rounded-full w-36 mt-1`}>
+              style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}
+              onPress={handleButtonPress}>
               <Text
                 style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
                 ኮርሱን ክፈት
               </Text>
             </TouchableOpacity>
           </View>
-        </View>
-        <View style={tw`border-b border-primary-7 mt-4 mb-4`} />
-        <View style={tw`flex flex-row justify-between items-center`}>
-          <Text style={tw`font-nokia-bold text-secondary-4 text-lg`}>
-            Discover Devotionals
-          </Text>
-          <TouchableOpacity
-            style={tw`border border-accent-6 px-4 py-1 rounded-4`}>
-            <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
-              All Devotionals
+          <View style={tw`border-b border-primary-7 mt-4 mb-2`} />
+          <View style={tw`flex flex-row justify-between items-center`}>
+            <Text
+              style={[
+                tw`font-nokia-bold text-secondary-4 text-lg`,
+                darkMode ? tw`text-primary-7` : null,
+              ]}>
+              Study this week's SSL
             </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={tw`flex flex-row flex-wrap h-88 gap-4 mt-4`}>
-          <View
-            style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
-            <ImageBackground
-              source={require('./../assets/bible.png')}
-              style={tw`overflow-hidden p-2`}>
-              <ArrowSquareUpRight
-                size={32}
-                weight="fill"
-                style={tw`text-secondary-1 self-end`}
-                color={'#FDFDFD'}
-              />
-              <Text style={tw`font-nokia-bold mt-22 text-primary-1 text-lg`}>
-                ታህሳስ 18
+            <TouchableOpacity
+              style={tw`border border-accent-6 px-4 py-1 rounded-4`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
+                All SSLs
               </Text>
-            </ImageBackground>
+            </TouchableOpacity>
           </View>
           <View
-            style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
-            <ImageBackground
-              source={require('./../assets/day22.png')}
-              style={tw`overflow-hidden p-2`}>
-              <ArrowSquareUpRight
-                size={32}
-                weight="fill"
-                style={tw`text-secondary-1 self-end`}
-                color={'#FDFDFD'}
+            style={tw`flex flex-row border border-accent-6 mt-4 rounded-4 p-2 gap-2`}>
+            <View style={tw`h-32 w-32`}>
+              <Image
+                source={require('./../assets/bible.png')}
+                style={tw`w-full h-full rounded-3`}
               />
-              <Text style={tw`font-nokia-bold mt-22 text-primary-1 text-lg`}>
-                ታህሳስ 17
+            </View>
+            <View style={tw`w-65%`}>
+              <Text
+                style={tw`font-nokia-bold text-accent-6 text-sm leading-tight`}>
+                የአጠናን ዘዴዎች
               </Text>
-            </ImageBackground>
+              <Text
+                style={[
+                  tw`font-nokia-bold text-secondary-6 text-lg leading-tight`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}>
+                ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
+              </Text>
+              <View style={tw`border-b border-accent-6 mt-1 w-[63%]`} />
+              <Text
+                style={[
+                  tw`font-nokia-bold text-secondary-5 text-xs`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}>
+                ጥቅምት 01 - ጥቅምት 07
+              </Text>
+              <TouchableOpacity
+                style={tw`bg-accent-6 px-4 py-1 rounded-full w-36 mt-1`}>
+                <Text
+                  style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                  ኮርሱን ክፈት
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <View
-            style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
-            <ImageBackground
-              source={require('./../assets/day4.jpeg')}
-              style={tw`overflow-hidden p-2`}>
-              <View>
+          <View style={tw`border-b border-primary-7 mt-4 mb-4`} />
+          <View style={tw`flex flex-row justify-between items-center`}>
+            <Text
+              style={[
+                tw`font-nokia-bold text-secondary-4 text-lg`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}>
+              Discover Devotionals
+            </Text>
+            <TouchableOpacity
+              style={tw`border border-accent-6 px-4 py-1 rounded-4`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-sm`}>
+                All Devotionals
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={tw`flex flex-row flex-wrap h-88 gap-4 mt-4`}>
+            <View
+              style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
+              <ImageBackground
+                source={require('./../assets/bible.png')}
+                style={tw`overflow-hidden p-2`}>
                 <ArrowSquareUpRight
                   size={32}
                   weight="fill"
@@ -225,17 +260,15 @@ const Home = () => {
                   color={'#FDFDFD'}
                 />
                 <Text style={tw`font-nokia-bold mt-22 text-primary-1 text-lg`}>
-                  ታህሳስ 16
+                  ታህሳስ 18
                 </Text>
-              </View>
-            </ImageBackground>
-          </View>
-          <View
-            style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
-            <ImageBackground
-              source={require('./../assets/day1.jpeg')}
-              style={tw`overflow-hidden p-2`}>
-              <View>
+              </ImageBackground>
+            </View>
+            <View
+              style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
+              <ImageBackground
+                source={require('./../assets/day22.png')}
+                style={tw`overflow-hidden p-2`}>
                 <ArrowSquareUpRight
                   size={32}
                   weight="fill"
@@ -243,14 +276,52 @@ const Home = () => {
                   color={'#FDFDFD'}
                 />
                 <Text style={tw`font-nokia-bold mt-22 text-primary-1 text-lg`}>
-                  ታህሳስ 15
+                  ታህሳስ 17
                 </Text>
-              </View>
-            </ImageBackground>
+              </ImageBackground>
+            </View>
+            <View
+              style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
+              <ImageBackground
+                source={require('./../assets/day4.jpeg')}
+                style={tw`overflow-hidden p-2`}>
+                <View>
+                  <ArrowSquareUpRight
+                    size={32}
+                    weight="fill"
+                    style={tw`text-secondary-1 self-end`}
+                    color={'#FDFDFD'}
+                  />
+                  <Text
+                    style={tw`font-nokia-bold mt-22 text-primary-1 text-lg`}>
+                    ታህሳስ 16
+                  </Text>
+                </View>
+              </ImageBackground>
+            </View>
+            <View
+              style={tw`w-[47.5%] h-[45%] flex flex-col gap-8 rounded-2 overflow-hidden`}>
+              <ImageBackground
+                source={require('./../assets/day1.jpeg')}
+                style={tw`overflow-hidden p-2`}>
+                <View>
+                  <ArrowSquareUpRight
+                    size={32}
+                    weight="fill"
+                    style={tw`text-secondary-1 self-end`}
+                    color={'#FDFDFD'}
+                  />
+                  <Text
+                    style={tw`font-nokia-bold mt-22 text-primary-1 text-lg`}>
+                    ታህሳስ 15
+                  </Text>
+                </View>
+              </ImageBackground>
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 };
 
