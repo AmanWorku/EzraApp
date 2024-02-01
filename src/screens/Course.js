fa268fa16867797a70a148821ddf792c6b592c36
AmanWorku <amexworku2014@gmail.com>
2024-02-01 12:43:42 +0300

    Update code segments from course and course content

--- src/screens/Course.js
+++ src/screens/Course.js
@@ -20,10 +20,12 @@ import {
 import tw from './../../tailwind';
 import {useGetCoursesQuery} from './../services/api';
 import {useNavigation} from '@react-navigation/native';
+import {useSelector} from 'react-redux';
 const Course = () => {
   const {data: courses, error, isLoading, refetch} = useGetCoursesQuery();
   const [searchTerm, setSearchTerm] = useState('');
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const darkMode = useSelector(state => state.ui.darkMode);
 
   const onRefresh = useCallback(async () => {
     try {
@@ -60,79 +62,107 @@ const Course = () => {
   }
 
   return (
-    <SafeAreaView style={tw`flex mx-auto w-[92%]`}>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        refreshControl={
-          <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
-        }>
-        <View style={tw`flex flex-row justify-between my-4 text-secondary-6`}>
-          <List size={32} weight="bold" style={tw`text-secondary-6`} />
-          <Text style={tw`font-nokia-bold text-xl text-secondary-6`}>
-            Course
-          </Text>
-          <User size={32} weight="bold" style={tw`text-secondary-6`} />
-        </View>
-        <View>
-          <TextInput
-            placeholder="Search courses..."
-            value={searchTerm}
-            onChangeText={handleSearch}
-            style={tw`border border-primary-7 rounded px-4 py-2 font-nokia-bold`}
-          />
-        </View>
-        <View style={tw`flex flex-row justify-between mt-3 items-center`}>
-          <Text style={tw`font-nokia-bold text-accent-6 text-lg`}>
-            Popular Courses
-          </Text>
-          <View style={tw`flex flex-row justify-between items-center gap-2`}>
-            <Text style={tw`font-nokia-bold text-accent-6 text-lg`}>
-              Latest
+    <View style={darkMode ? tw`bg-secondary-9` : null}>
+      <SafeAreaView style={tw`flex mx-auto w-[92%]`}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+          }>
+          <View style={tw`flex flex-row justify-between my-4 text-secondary-6`}>
+            <List
+              size={32}
+              weight="bold"
+              style={[
+                tw`text-secondary-6`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}
+            />
+            <Text
+              style={[
+                tw`font-nokia-bold text-xl text-secondary-6`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}>
+              Course
             </Text>
-            <CaretCircleDown size={24} weight="fill" color={'#EA9215'} />
+            <User
+              size={32}
+              weight="bold"
+              style={[
+                tw`text-secondary-6`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}
+            />
           </View>
-        </View>
-        {courses &&
-          courses.map((course, index) => {
-            return (
-              <View
-                style={tw`border border-accent-6 my-2 rounded-4 p-2`}
-                key={index}>
-                <View style={tw`h-48`}>
-                  <Image
-                    source={{
-                      uri: `https://ezra-seminary-api.onrender.com/images/${course.image}`,
-                    }}
-                    style={tw`w-full h-full rounded-3`}
-                  />
-                </View>
-                <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
-                  የአጠናን ዘዴዎች
-                </Text>
-                <Text style={tw`font-nokia-bold text-secondary-6 text-2xl`}>
-                  {course.title}
-                </Text>
-                <View style={tw`flex flex-row items-center justify-between`}>
-                  <TouchableOpacity
-                    style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}
-                    onPress={() => handleButtonPress(course._id)}>
-                    <Text
-                      style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
-                      ኮርሱን ክፈት
-                    </Text>
-                  </TouchableOpacity>
-                  <View style={tw`flex flex-row items-center gap-1`}>
-                    <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
-                      5.0
-                    </Text>
-                    <Star size={22} weight="fill" color={'#EA9215'} />
+          <View>
+            <TextInput
+              placeholder="Search courses..."
+              value={searchTerm}
+              onChangeText={handleSearch}
+              style={[
+                tw`border border-primary-7 rounded px-4 py-2 font-nokia-bold`,
+                darkMode ? tw`text-primary-1` : null,
+              ]}
+              placeholderTextColor={darkMode ? '#898989' : '#AAB0B4'}
+            />
+          </View>
+          <View style={tw`flex flex-row justify-between mt-3 items-center`}>
+            <Text style={tw`font-nokia-bold text-accent-6 text-lg`}>
+              Popular Courses
+            </Text>
+            <View style={tw`flex flex-row justify-between items-center gap-2`}>
+              <Text style={tw`font-nokia-bold text-accent-6 text-lg`}>
+                Latest
+              </Text>
+              <CaretCircleDown size={24} weight="fill" color={'#EA9215'} />
+            </View>
+          </View>
+          {courses &&
+            courses.map((course, index) => {
+              return (
+                <View
+                  style={tw`border border-accent-6 my-2 rounded-4 p-2`}
+                  key={index}>
+                  <View style={tw`h-48`}>
+                    <Image
+                      source={{
+                        uri: `https://ezra-seminary-api.onrender.com/images/${course.image}`,
+                      }}
+                      style={tw`w-full h-full rounded-3`}
+                    />
+                  </View>
+                  <Text style={tw`font-nokia-bold text-accent-6 text-xl mt-2`}>
+                    የአጠናን ዘዴዎች
+                  </Text>
+                  <Text
+                    style={[
+                      tw`font-nokia-bold text-secondary-6 text-2xl`,
+                      darkMode ? tw`text-primary-3` : null,
+                    ]}>
+                    {course.title}
+                  </Text>
+                  <View style={tw`flex flex-row items-center justify-between`}>
+                    <TouchableOpacity
+                      style={tw`bg-accent-6 px-4 py-2 rounded-full w-36 mt-2`}
+                      onPress={() => handleButtonPress(course._id)}>
+                      <Text
+                        style={tw`text-primary-1 font-nokia-bold text-sm text-center`}>
+                        ኮርሱን ክፈት
+                      </Text>
+                    </TouchableOpacity>
+                    <View style={tw`flex flex-row items-center gap-1`}>
+                      <Text style={tw`font-nokia-bold text-accent-6 text-2xl `}>
+                        5.0
+                      </Text>
+                      <Star size={22} weight="fill" color={'#EA9215'} />
+                    </View>
                   </View>
                 </View>
-              </View>
-            );
-          })}
-      </ScrollView>
-    </SafeAreaView>
+              );
+            })}
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 };
 
--- src/screens/CoursePages/CourseContent.js
+++ src/screens/CoursePages/CourseContent.js
@@ -5,18 +5,21 @@ import {
   ScrollView,
   ImageBackground,
   TouchableOpacity,
+  RefreshControl,
 } from 'react-native';
 import React, {useState} from 'react';
 import tw from './../../../tailwind';
 import {useGetCourseByIdQuery} from './../../services/api';
 import {useNavigation} from '@react-navigation/native';
 import {ArrowSquareLeft, CheckCircle, Circle} from 'phosphor-react-native';
+import {useSelector} from 'react-redux';
 
 const CourseContent = ({route}) => {
   const {courseId} = route.params;
   const navigation = useNavigation();
   const [activeIndex, setActiveIndex] = useState(0);
   const [unlockedIndex, setUnlockedIndex] = useState(0);
+  const darkMode = useSelector(state => state.ui.darkMode);
   const {
     data: courseData,
     error,
@@ -47,86 +50,110 @@ const CourseContent = ({route}) => {
   };
 
   return (
-    <SafeAreaView>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={tw`flex-1 h-70`}>
-          <ImageBackground
-            source={require('./../../assets/bible2.jpeg')}
-            style={tw`flex-5`}>
-            <TouchableOpacity onPress={backButtonPress}>
-              <ArrowSquareLeft
-                size={36}
-                weight="fill"
-                color={'#EA9215'}
-                style={tw`px-8 mt-4`}
-              />
-            </TouchableOpacity>
-          </ImageBackground>
-          <View
-            style={tw`flex-1 bg-primary-7 flex-row gap-2 justify-center items-center`}>
-            <View style={tw`p-1 bg-accent-6 rounded-1`}>
-              <Text style={tw`font-nokia-bold text-primary-1`}>10%</Text>
-            </View>
-            <Text style={tw`font-nokia-bold text-secondary-6 text-sm`}>
-              Pass 100% of your lessons to complete this course
-            </Text>
-          </View>
-        </View>
-        <View style={tw`flex mx-auto w-[92%]`}>
-          <Text style={tw`font-nokia-bold text-secondary-6 text-2xl mt-2`}>
-            {courseData?.title}
-          </Text>
-          <Text
-            style={tw`font-nokia-bold text-secondary-6 text-justify text-lg leading-tight my-3`}>
-            {'    '}
-            {courseData?.description}
-          </Text>
-          <View style={tw`flex flex-row items-center`}>
-            <View style={tw`border-b-4 border-accent-6`}>
-              <Text style={tw`py-2 font-nokia-bold text-secondary-6`}>
-                ትምህርቶች {currentDataNumber}/{totalDataNumber}
+    <View style={darkMode ? tw`bg-secondary-9 h-100%` : null}>
+      <SafeAreaView>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={tw`flex-1 h-70`}>
+            <ImageBackground
+              source={require('./../../assets/bible2.jpeg')}
+              style={tw`flex-5`}>
+              <TouchableOpacity onPress={backButtonPress}>
+                <ArrowSquareLeft
+                  size={36}
+                  weight="fill"
+                  color={'#EA9215'}
+                  style={tw`px-8 mt-4`}
+                />
+              </TouchableOpacity>
+            </ImageBackground>
+            <View
+              style={[
+                tw`flex-1 bg-primary-7 flex-row gap-2 justify-center items-center`,
+                darkMode ? tw`bg-secondary-8` : null,
+              ]}>
+              <View style={tw`p-1 bg-accent-6 rounded-1`}>
+                <Text style={tw`font-nokia-bold text-primary-1`}>10%</Text>
+              </View>
+              <Text
+                style={[
+                  tw`font-nokia-bold text-secondary-6 text-sm`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}>
+                Pass 100% of your lessons to complete this course
               </Text>
             </View>
-            <View style={tw`border-b border-accent-6 h-4 flex-grow mt-5`} />
           </View>
-          {data.map((chapter, index) => {
-            const unlocked = isSlideUnlocked(index);
-            return (
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('SlideSample1', {
-                    chapterTitle: chapter.chapter,
-                    courseDescription: 'Short description of the course...',
-                    chapterId: chapter._id,
-                    CId: courseId,
-                  });
-                  updateIndex(index);
-                }}
-                key={index}>
-                <View
-                  style={tw`flex flex-row justify-between px-4 py-2 items-center`}>
-                  <View style={tw`flex`}>
-                    <Text style={tw`font-nokia-bold text-secondary-6 text-lg`}>
-                      {chapter.chapter}
-                      {/* <Text>ID</Text> {courseId} */}
-                    </Text>
-                    <Text style={tw`font-nokia-bold text-accent-6 text-xs`}>
-                      15/15 Slides
-                    </Text>
+          <View style={tw`flex mx-auto w-[92%]`}>
+            <Text
+              style={[
+                tw`font-nokia-bold text-secondary-6 text-2xl mt-2`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}>
+              {courseData?.title}
+            </Text>
+            <Text
+              style={[
+                tw`font-nokia-bold text-secondary-6 text-justify text-lg leading-tight my-3`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}>
+              {'    '}
+              {courseData?.description}
+            </Text>
+            <View style={tw`flex flex-row items-center`}>
+              <View style={tw`border-b-4 border-accent-6`}>
+                <Text
+                  style={[
+                    tw`py-2 font-nokia-bold text-secondary-6`,
+                    darkMode ? tw`text-primary-3` : null,
+                  ]}>
+                  ትምህርቶች {currentDataNumber}/{totalDataNumber}
+                </Text>
+              </View>
+              <View style={tw`border-b border-accent-6 h-4 flex-grow mt-5`} />
+            </View>
+            {data.map((chapter, index) => {
+              const unlocked = isSlideUnlocked(index);
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('SlideSample1', {
+                      chapterTitle: chapter.chapter,
+                      courseDescription: chapter.description,
+                      chapterId: chapter._id,
+                      CId: courseId,
+                    });
+                    updateIndex(index);
+                  }}
+                  key={index}>
+                  <View
+                    style={tw`flex flex-row justify-between px-4 py-2 items-center`}>
+                    <View style={tw`flex`}>
+                      <Text
+                        style={[
+                          tw`font-nokia-bold text-secondary-6 text-lg`,
+                          darkMode ? tw`text-primary-3` : null,
+                        ]}>
+                        {chapter.chapter}
+                        {/* <Text>ID</Text> {courseId} */}
+                      </Text>
+                      <Text style={tw`font-nokia-bold text-accent-6 text-xs`}>
+                        15/15 Slides
+                      </Text>
+                    </View>
+                    {unlocked ? (
+                      <CheckCircle size={20} weight="fill" color={'#EA9215'} />
+                    ) : (
+                      <Circle size={20} color={'#EA9215'} />
+                    )}
                   </View>
-                  {unlocked ? (
-                    <CheckCircle size={20} weight="fill" color={'#EA9215'} />
-                  ) : (
-                    <Circle size={20} color={'#EA9215'} />
-                  )}
-                </View>
-                <View style={tw`border-b border-accent-6 h-1 flex-grow`} />
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+                  <View style={tw`border-b border-accent-6 h-1 flex-grow`} />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 };
 
