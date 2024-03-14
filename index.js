074b87eb124d89bcb0e6cfba86639b6e6ebd3dd6
AmanWorku <amexworku2014@gmail.com>
2024-03-14 11:21:44 +0300

    Refactor ErrorBoundary component and remove unnecessary comments

--- index.js
+++ index.js
@@ -5,11 +5,10 @@
 import React from 'react';
 import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
-import ErrorBoundary from './src/components/ErrorBoundary'; // Import the ErrorBoundary component
+import ErrorBoundary from './src/components/ErrorBoundary';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 
-// Ignore specific logs
 LogBox.ignoreLogs(['ViewPropTypes will be removed', 'Carousel.propTypes']);
 
 const AppWithErrorBoundary = () => {
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -8,24 +8,20 @@ class ErrorBoundary extends Component {
   }
 
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
     return {hasError: true};
   }
 
   componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
     console.error('Error caught by ErrorBoundary:', error, errorInfo);
   }
 
   handleRetry = () => {
-    // Callback function to retry fetching the data
-    this.setState({hasError: false}); // Reset the error state
-    this.props.retryFetch(); // Call the retry function passed as props
+    this.setState({hasError: false});
+    this.props.retryFetch();
   };
 
   render() {
     if (this.state.hasError) {
-      // Fallback UI if an error occurs
       return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <Text>An error occurred. Please try again later.</Text>
@@ -33,8 +29,6 @@ class ErrorBoundary extends Component {
         </View>
       );
     }
-
-    // If there's no error, render the children components as usual
     return this.props.children;
   }
 }
