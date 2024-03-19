b457320d3d45f22d90e0656459a0fa794336c045
AmanWorku <amexworku2014@gmail.com>
2024-03-19 16:50:27 +0300

    Fix bug in login functionality

--- src/screens/Settings/UserProfileUpdateScreen.js
+++ src/screens/Settings/UserProfileUpdateScreen.js
@@ -12,17 +12,24 @@ import {
 import {useDispatch, useSelector} from 'react-redux';
 import {updateUser} from '../../redux/authSlice';
 import tw from './../../../tailwind';
-import {ArrowSquareLeft} from 'phosphor-react-native';
+import {
+  ArrowSquareLeft,
+  UserCircle,
+  EnvelopeSimple,
+  Lock,
+  Eye,
+} from 'phosphor-react-native';
 import bible from '../../assets/bible.png';
 
 const UserProfileUpdateScreen = ({navigation}) => {
-  // States for user inputs
   const dispatch = useDispatch();
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const darkMode = useSelector(state => state.ui.darkMode);
   const currentUser = useSelector(state => state.auth);
   const [avatarPreview, setAvatarPreview] = useState(null);
@@ -40,6 +47,14 @@ const UserProfileUpdateScreen = ({navigation}) => {
       );
     }
   }, [currentUser]);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
 
   const handleUpdateUser = () => {
     const userData = {firstName, lastName, email, password};
@@ -62,53 +77,159 @@ const UserProfileUpdateScreen = ({navigation}) => {
         tw`flex-1 items-center px-4 bg-primary-1`,
         darkMode && tw`bg-secondary-9`,
       ]}>
-      <View style={tw`w-92% mt-4 p-4 rounded-4`}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <ArrowSquareLeft
-            size={36}
-            weight="fill"
-            color={'#EA9215'}
-            style={tw`my-4`}
-          />
-        </TouchableOpacity>
-        <Text style={styles.label}>First Name</Text>
-        <TextInput
-          style={styles.input}
-          value={currentUser && currentUser.user && currentUser.user.firstName}
-          onChangeText={setFirstName}
-          placeholder="Enter first name"
-        />
-
-        <Text style={styles.label}>Last Name</Text>
-        <TextInput
-          style={styles.input}
-          value={currentUser && currentUser.user && currentUser.user.lastName}
-          onChangeText={setLastName}
-          placeholder="Enter last name"
-        />
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          value={currentUser && currentUser.user && currentUser.user.email}
-          onChangeText={setEmail}
-          placeholder="Enter email"
-          keyboardType="email-address"
-        />
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Enter password"
-          secureTextEntry
-        />
-        <TouchableOpacity
-          style={tw`px-4 py-2 rounded-4 bg-accent-6 self-center mt-4`}
-          onPress={handleUpdateUser}>
-          <Text style={tw`font-nokia-bold text-sm text-primary-1`}>
-            Update Profile
-          </Text>
-        </TouchableOpacity>
+      <View style={tw`flex flex-col gap-4`}>
+        <View style={tw`flex flex-row mb-2 justify-between`}>
+          <View
+            style={[
+              tw`flex flex-row items-center gap-2 w-48% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`,
+              darkMode ? tw`bg-secondary-6` : null,
+            ]}>
+            <UserCircle
+              size={20}
+              style={[
+                tw`text-secondary-5`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}
+            />
+            <TextInput
+              placeholder="First Name"
+              keyboardType="default"
+              value={firstName}
+              onChangeText={setFirstName}
+              style={[
+                tw`font-nokia-bold text-sm text-secondary-6 w-100%`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}
+              placeholderTextColor={darkMode ? '#AAAAAA' : '#AAB0B4'}
+            />
+          </View>
+          <View
+            style={[
+              tw`flex flex-row items-center gap-2 w-48% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`,
+              darkMode ? tw`bg-secondary-6` : null,
+            ]}>
+            <UserCircle
+              size={20}
+              style={[
+                tw`text-secondary-5`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}
+            />
+            <TextInput
+              placeholder="Last Name"
+              keyboardType="default"
+              value={lastName}
+              onChangeText={setLastName}
+              style={[
+                tw`font-nokia-bold text-sm text-secondary-6 w-100%`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}
+              placeholderTextColor={darkMode ? '#AAAAAA' : '#AAB0B4'}
+            />
+          </View>
+        </View>
+        <View style={tw`mb-2`}>
+          <View
+            style={[
+              tw`flex flex-row items-center gap-2 w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`,
+              darkMode ? tw`bg-secondary-6` : null,
+            ]}>
+            <EnvelopeSimple
+              size={20}
+              style={[
+                tw`text-secondary-5`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}
+            />
+            <TextInput
+              placeholder="Email address"
+              keyboardType="email-address"
+              value={email}
+              onChangeText={setEmail}
+              style={[
+                tw`font-nokia-bold text-sm text-secondary-6 w-80%`,
+                darkMode ? tw`text-primary-3` : null,
+              ]}
+              placeholderTextColor={darkMode ? '#AAAAAA' : '#AAB0B4'}
+            />
+          </View>
+        </View>
+        <View style={tw`mb-2`}>
+          <View
+            style={[
+              tw`flex flex-row items-center justify-between gap-2 w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`,
+              darkMode ? tw`bg-secondary-6` : null,
+            ]}>
+            <View style={tw`flex flex-row items-center gap-2`}>
+              <Lock
+                size={20}
+                style={[
+                  tw`text-secondary-5`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}
+              />
+              <TextInput
+                placeholder="Password"
+                secureTextEntry={showPassword}
+                keyboardType="default"
+                value={password}
+                onChangeText={setPassword}
+                style={[
+                  tw`font-nokia-bold text-sm text-secondary-6 w-80%`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}
+                placeholderTextColor={darkMode ? '#AAAAAA' : '#AAB0B4'}
+              />
+            </View>
+            <TouchableOpacity onPress={toggleShowPassword}>
+              <Eye
+                size={20}
+                style={[
+                  tw`text-secondary-5`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={tw`mb-2`}>
+          <View
+            style={[
+              tw`flex flex-row items-center justify-between gap-2 w-100% h-12 bg-primary-4 border border-secondary-3 rounded-2 px-4`,
+              darkMode ? tw`bg-secondary-6` : null,
+            ]}>
+            <View style={tw`flex flex-row items-center gap-2`}>
+              <Lock
+                size={20}
+                style={[
+                  tw`text-secondary-5`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}
+              />
+              <TextInput
+                placeholder="Confirm Password"
+                secureTextEntry={showConfirmPassword}
+                keyboardType="default"
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                style={[
+                  tw`font-nokia-bold text-sm text-secondary-6 w-80%`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}
+                placeholderTextColor={darkMode ? '#AAAAAA' : '#AAB0B4'}
+              />
+            </View>
+            <TouchableOpacity onPress={toggleShowConfirmPassword}>
+              <Eye
+                size={20}
+                style={[
+                  tw`text-secondary-5`,
+                  darkMode ? tw`text-primary-3` : null,
+                ]}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
