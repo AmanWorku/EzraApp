68c0fca13aeaa19611e319c2b1ccf91bce709ff5
AmanWorku <amexworku2014@gmail.com>
2024-04-19 15:35:01 +0300

    Refactor Reveal component to use multiple FlipCards for each reveal item

--- src/screens/CoursePages/Types/Reveal.js
+++ src/screens/CoursePages/Types/Reveal.js
@@ -1,38 +1,47 @@
 import React, {useState} from 'react';
-import {View, Text, Dimensions} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import FlipCard from 'react-native-flip-card';
 import tw from '../../../../tailwind';
 
 const Reveal = ({value}) => {
-  const [isFlipped, setIsFlipped] = useState(false);
+  const [flip, setFlip] = useState(value.map(() => false));
 
-  const handleFlip = () => {
-    setIsFlipped(!isFlipped);
+  const handleFlip = index => {
+    setFlip(prevFlip =>
+      prevFlip.map((isFlipped, i) => (i === index ? !isFlipped : isFlipped)),
+    );
   };
 
   return (
-    <FlipCard
-      style={tw`w-full h-$half rounded-lg bg-white shadow`}
-      friction={6}
-      perspective={1000}
-      flipHorizontal={true}
-      flipVertical={false}
-      flip={isFlipped}
-      onClick={handleFlip}>
-      {/* Face Side */}
-      <View style={tw`flex-1 items-center justify-center`}>
-        <Text style={tw`font-nokia-bold text-lg text-primary-1 px-4`}>
-          {value.title}
-        </Text>
-      </View>
-      {/* Back Side */}
-      <View
-        style={tw`flex-1 items-center justify-center bg-accent-6 rounded-lg`}>
-        <Text style={tw`font-nokia-bold text-lg text-primary-1 px-4`}>
-          {value.content}
-        </Text>
-      </View>
-    </FlipCard>
+    <>
+      {value.map((revealItem, index) => (
+        <FlipCard
+          key={index}
+          style={tw`w-full h-$half rounded-lg shadow`}
+          friction={6}
+          perspective={1000}
+          flipHorizontal={false}
+          flipVertical={true}
+          flip={flip[index]}>
+          {/* Face Side */}
+          <TouchableOpacity
+            style={tw`flex-1 items-center justify-center border border-accent-6 bg-accent-10 bg-opacity-50 rounded py-8`}
+            onPress={() => handleFlip(index)}>
+            <Text style={tw`font-nokia-bold text-xl text-primary-1`}>
+              {revealItem.title}
+            </Text>
+          </TouchableOpacity>
+          {/* Back Side */}
+          <TouchableOpacity
+            style={tw`flex-1 items-center justify-center border border-accent-6 bg-accent-10 bg-opacity-80 rounded`}
+            onPress={() => handleFlip(index)}>
+            <Text style={tw`font-nokia-bold text-lg text-primary-1 p-4`}>
+              {revealItem.content}
+            </Text>
+          </TouchableOpacity>
+        </FlipCard>
+      ))}
+    </>
   );
 };
 
