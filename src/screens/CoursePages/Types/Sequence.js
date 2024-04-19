baa9f44fc1c0837c97ebbd41d6c993e23aff02c5
AmanWorku <amexworku2014@gmail.com>
2024-04-19 13:07:53 +0300

    Add Sequence component to CoursePages/Types directory

---
+++ src/screens/CoursePages/Types/Sequence.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Sequence = ({value}) => {
+  return (
+    <View>
+      <Text>Sequence</Text>
+    </View>
+  );
+};
+
+export default Sequence;
