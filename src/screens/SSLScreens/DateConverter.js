bb74eafcc6086b4c1275864031c65507e75c9ce8
AmanWorku <amexworku2014@gmail.com>
2025-02-11 20:41:00 +0300

    Update code to display

--- src/screens/SSLScreens/DateConverter.js
+++ src/screens/SSLScreens/DateConverter.js
@@ -2,12 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {EthDateTime} from 'ethiopian-calendar-date-converter';
 import {Text} from 'react-native';
-import {useSelector} from 'react-redux';
 import tw from './../../../tailwind'; // Ensure this path is correct
 
-const DateConverter = ({gregorianDate, textStyle}) => {
-  const darkMode = useSelector(state => state.ui.darkMode);
-
+const DateConverter = ({gregorianDate}) => {
   // Split the date string and convert to numbers
   const [day, month, year] = gregorianDate.split('/').map(Number);
 
@@ -16,10 +13,20 @@ const DateConverter = ({gregorianDate, textStyle}) => {
     return <Text>Invalid date format: {gregorianDate}</Text>;
   }
 
+  // Adjust the date for the Ethiopian week transition
+  const date = new Date(year, month - 1, day);
+  const dayOfWeek = date.getDay();
+
+  // If it's Saturday and past 6 PM, move to the next day
+  if (dayOfWeek === 6) {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 18) {
+      date.setDate(date.getDate() + 1);
+    }
+  }
+
   // Convert to Ethiopian date
-  const ethDateTime = EthDateTime.fromEuropeanDate(
-    new Date(year, month - 1, day + 1),
-  );
+  const ethDateTime = EthDateTime.fromEuropeanDate(date);
   const ethiopianMonthNames = [
     'መስከረም',
     'ጥቅምት',
@@ -39,7 +46,7 @@ const DateConverter = ({gregorianDate, textStyle}) => {
   const ethiopianMonthName = ethiopianMonthNames[ethDateTime.month - 1];
 
   return (
-    <Text style={tw`${textStyle}`}>
+    <Text style={tw`font-nokia-bold text-accent-6`}>
       {ethiopianMonthName} {ethDateTime.date}
     </Text>
   );
